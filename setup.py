55cb7d553a9d0d19eba9d194e6dbc3e52eb08a0f
Daniel Glunz <dannyglunz@gmail.com>
2019-09-06 17:56:24 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -11,6 +11,7 @@ setup(name='tap-postgres',
       install_requires=[
           'singer-python==5.3.1',
           'psycopg2==2.7.4',
+          'psycopg2-binary==2.7.4',
           'strict-rfc3339==0.7',
           'nose==1.3.7'
       ],
